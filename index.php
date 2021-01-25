3f38abcecefd5f3e9f7edf3eccde3497a37387cb
@ChristianAfreh <afrehchristian@gmail.com>
2021-01-25 09:29:22 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <title>Christian Afreh - Rock Paper Scissors</title>
+                <?php require_once "ibootstrap.php"; ?>
+        
+        </head>
+        <body>
+                <div class="container">
+                    <h1>Welcome to Rock Paper Scissors</h1>
+                    <p>
+                        <a href="login.php">Please Log In</a>
+                    </p>
+                Attempt to go to
+                <a href="game.php">game.php</a> without logging in - it should
+                fail an error message.
+                
+
+                </div>
+        </body>
+        
+</html>
